614335367705c253f73561d92c75482b58a65dc9
Benny <benny08118@gmail.com>
2020-03-11 18:01:50 +0800

    add google vision API 01

---
+++ google-vision-api/src/main/java/bennyGoogle/google_vision_api/excelHelp.java
@@ -0,0 +1,12 @@
+package bennyGoogle.google_vision_api;
+
+import java.io.FileOutputStream;
+
+public class excelHelp extends visionHelp {
+
+	@Override
+	public void writeData(String outDir,String[] inputFileNames,List<List<String[]>> dataList) {
+		
+		
+	}
+}
